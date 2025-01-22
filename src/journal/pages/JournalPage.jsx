e3ead1eea257984475dbacf93d57cd85900774c4
CordobaFacundo <facundo.cordoba2012@gmail.com>
2025-01-22 15:13:39 -0300

    Navbar + Sidebar

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,17 @@
 import { Typography } from '@mui/material'
 import React from 'react'
+import { JournalLayout } from '../layout/JournalLayout'
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>Esse ad voluptate consequat proident aliquip est in eiusmod sint pariatur elit. Velit aute duis eiusmod sint ipsum eu anim laboris est. Cillum amet duis officia do irure est sit tempor consequat nostrud. Anim ipsum duis proident duis magna consectetur pariatur sunt. Excepteur do adipisicing irure ex sint culpa consectetur incididunt aliqua ea officia reprehenderit nostrud. Velit tempor esse laborum tempor excepteur labore labore nisi dolor.</Typography>
+
+      {/* NothinSelected */}
+
+      {/* Noteview */}
+    
+    
+    </JournalLayout>
   )
 }
