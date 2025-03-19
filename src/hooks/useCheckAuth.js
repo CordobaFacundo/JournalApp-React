8e1642bb5b96e964130f2fd10dc8a6d3f0d37db1
CordobaFacundo <facundo.cordoba2012@gmail.com>
2025-03-19 17:23:26 -0300

    load notes Firebase

--- src/hooks/useCheckAuth.js
+++ src/hooks/useCheckAuth.js
@@ -3,6 +3,7 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { useDispatch, useSelector } from 'react-redux';
 import { login, logout } from '../store/auth/authSlice';
 import { FirebaseAuth } from '../firebase/config';
+import { startLoadingNotes } from '../store/journal';
 
 export const useCheckAuth = () => {
 
@@ -13,7 +14,8 @@ export const useCheckAuth = () => {
       onAuthStateChanged( FirebaseAuth, async( user ) => {
         if( !user ) return dispatch(logout())
         const { uid, email, displayName, photoURL } = user;
-        dispatch(login({uid, email, displayName, photoURL}))
+        dispatch(login({uid, email, displayName, photoURL}));
+        dispatch(startLoadingNotes());
       })
     }, [])
 
