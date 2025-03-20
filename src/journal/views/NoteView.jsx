e00c6e73688a5609c5550d5cf74a98fa4d015f8f
CordobaFacundo <facundo.cordoba2012@gmail.com>
2025-03-20 18:49:21 -0300

    sweetalert2 + moment

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,46 +1,55 @@
+import React, { useEffect, useMemo } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { SaveOutlined } from '@mui/icons-material'
 import { Button, Grid, TextField, Typography } from '@mui/material'
-import React, { useEffect, useMemo } from 'react'
 import { ImageGallery } from '../components'
-import { useDispatch, useSelector } from 'react-redux'
 import { useForm } from '../../hooks/useForm'
 import { setActiveNote, startSaveNote } from '../../store/journal'
+import Swal from 'sweetalert2'
+import 'sweetalert2/dist/sweetalert2.css'
+import moment from 'moment'
 
 export const NoteView = () => {
 
     const dispatch = useDispatch();
 
-    const { active: note } = useSelector( state => state.journal );
+    const { active: note, messageSaved, isSaving } = useSelector( state => state.journal );
 
     const { body, title, date, onInputChange, formState } = useForm( note );
-    
-    const dateString = useMemo( () => {
-        const newDate = new Date(date);
-        return newDate.toUTCString();
-    }, [date])
+
+    const dateString = useMemo(() => {
+        return moment(date).utc().format("ddd, DD MMM YYYY HH:mm:ss");
+    }, [date]);
 
     useEffect(() => {
       dispatch( setActiveNote(formState) );
     }, [formState])
 
+    useEffect(() => {
+      if( messageSaved.length > 0 ){
+        Swal.fire('Note updated', messageSaved, 'success');
+      }
+    }, [messageSaved])
+    
     const onSaveNote = () => {
         dispatch( startSaveNote() );
     }
     
-
+    
     return (
         <Grid container direction='row' justifyContent='space-between' alignItems='center' sx={{ mb: 1 }}>
             <Grid item>
-                <Typography fontSize={39} fontWeight='light'>{ dateString }</Typography>
+                <Typography fontSize={25} fontWeight='light'>{ dateString }</Typography>
             </Grid>
             <Grid item>
                 <Button 
+                    disabled={isSaving}
                     onClick={onSaveNote}
                     color='primary' 
                     sx={{ padding: 2 }}
                     >
                     <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
-                    Guardar
+                    Save
                 </Button>
             </Grid>
 
